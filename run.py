59ddafc001b96b1bb0303877f7622d27f8f28ce0
nearjay06 <nearjoan2018@gmail.com>
2018-10-24 15:05:11 +0300

    modify run file

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__) 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+from flask import Flask,app
+app = Flask(__name__)
 
    
 if __name__ =='__main__':
